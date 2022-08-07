bad0d411b39c17c63e6834d68ad34a7901cab486
Hasham <hashamsp36@gmail.com>
2022-08-07 13:21:33 -0400

    attempt to connect bluetooth device

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Timers;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
 using InTheHand.Net.Sockets;
 
 namespace BTConnector
@@ -9,6 +12,9 @@ namespace BTConnector
     {
         static void Main(string[] args)
         {
+
+            Dictionary<string, BluetoothDeviceInfo> devices = new Dictionary<string, BluetoothDeviceInfo>();
+            BluetoothClient client = new BluetoothClient();
 
             //The main console ui
             while (true)
@@ -21,41 +27,41 @@ namespace BTConnector
 
                 if(command == 'x')
                 {
+                    client.Close();
                     break;
                 } else if (command == 'l')
                 {
-                    startDeviceListener();
+                    scanAndGetAllDevices(client, devices);
                 } else if(command == 'c')
                 {
+                    scanAndGetAllDevices(client, devices);
                     Console.WriteLine("Type the name of device you want to pair with");
-                    string inputedDeviceName = Console.ReadLine(); 
+                    string inputedDeviceName = Console.ReadLine();
                     // TODO: make use of this device name to attemp to pair with the device found in listening or start listening again
+
+                    // TODO: fix code for connecting to bluetooth device
+                    //if(devices.TryGetValue(inputedDeviceName, out BluetoothDeviceInfo device))
+                    //{
+                    //    BluetoothEndPoint ep = new BluetoothEndPoint(device.DeviceAddress, serviceClass);
+                    //    client.Connect(ep);
+                    //}
 
                 }
             }
         }
 
 
-        private static void startDeviceListener()
+        private static void scanAndGetAllDevices(BluetoothClient client, Dictionary<string, BluetoothDeviceInfo> devices)
         {
-
-
-
-            BluetoothClient client = new BluetoothClient();
-
-            Console.WriteLine("Listening for bluetooth devices");
-
-
-
+            Console.WriteLine("Discovering Devices...");
             foreach (var dev in client.DiscoverDevices())
             {
                 Console.WriteLine("hello device:" + "-" + dev.DeviceAddress + "-" + dev.DeviceName);
+                devices.Add(dev.DeviceName, dev);
             }
-
-
-            client.Close();
-
         }
+
+
 
     }
   
